44be3d74a82df59fcf2c6384aa81f9651fa0efc5
Mohamed Elkarim <mohdawad2010@hotmail.com>
2018-11-23 23:05:21 -0500

    creating fronend

---
+++ src/Dress.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import socketIOClient from 'socket.io-client';
+
+class Dress extends Component {
+  constructor() {
+    super()
+    this.state = {
+      endpoint: 'http://localhost: 3000',
+      dress: {}
+    }
+  }
+
+  send = () => {
+    const socket = socketIOClient(this.state.endpoint)
+
+    socket.emit('dress update', this.state.dress)
+  }
+
+  setDress = (dress) => {
+    this.setState({dress})
+  }
+
+  render() {
+    const socket= socketIOClient(this.state.endpoint)
+
+    socket.on('dress update', (dress) => {
+      document.body.innerHTML = dress;
+    })
+    return(
+      <div>
+        <input type='text' name='name' value={this.state.dress} onChange={this.send()} />
+      </div>
+    )
+  }
+
+}
+
+export default Dress;
---
+++ src/LiveShow.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Dress from './Dress';
+
+class LiveShow extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+      <Dress />
+      </div>
+    )
+  }
+}
+
+export default LiveShow;
